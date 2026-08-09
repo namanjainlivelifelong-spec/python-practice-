e56355d8c7e49f624b336d2d537d9f9c83548cec
Naman Jain <namanjainlivelifelong@gmail.com>
2026-08-09 17:49:00 +0530

    Added new Python practice

---
+++ reversed_table.py
@@ -0,0 +1,4 @@
+n = int(input("enter the no. = "))
+for i in range(10,0,-1):
+  print(f"{n}X{i}= {n*i}")
+print()  
--- star_sapce_print.py
+++ star_sapce_print.py
@@ -1,3 +1,10 @@
 n =int(input("enter the no. = "))
 for i in range(1,n+1):
-    
+    for j in range(1, n+1):
+        if(i==1 or i==n):
+            print("*",end= "")
+        elif(j==1 or j==n):
+            print("*",end= "")
+        else: 
+            print(" ",end= "") 
+    print()        
